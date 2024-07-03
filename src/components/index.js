b6b2f5f4ee290178dfd412ab98b3d440793fa389
Kshitiz <kshitizagarwa2405@gmail.com>
2024-07-04 01:39:59 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,30 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./Container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
+import PostForm from "./post-form/PostForm";
+import Protected from "./AuthLayout";
+import Input from "./Input";
+import Login from "./Login";
+import PostCard from "./PostCard";
+import RTE from "./RTE";
+import Select from "./Select";
+import Signup from "./SignUp";
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  Button,
+  PostForm,
+  Protected,
+  Input,
+  Login,
+  PostCard,
+  RTE,
+  Select,
+  Signup,
+};
---
+++ src/store/postSlice.js
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  posts: [],
+};
+
+const postSlice = createSlice({
+  name: "posts",
+  initialState,
+  reducers: {
+    Posts: (state, action) => {
+      state.posts = action.payload;
+    },
+    addPost: (state, action) => {
+      state.posts.push(action.payload);
+    },
+    deletePost: (state, action) => {
+      state.posts = state.posts.filter((post) => post.$id !== action.payload);
+    },
+    editPost: (state, action) => {
+      state.posts = state.posts.map((post) =>
+        post.$id === action.payload.$id
+          ? {
+              ...post,
+              title: action.payload.title,
+              featuredImage: action.payload.featuredImage,
+              status: action.payload.status,
+              content: action.payload.content,
+            }
+          : post
+      );
+    },
+  },
+});
+
+export const { addPost, editPost, deletePost, Posts } = postSlice.actions;
+
+export default postSlice.reducer;
